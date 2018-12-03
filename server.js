8066284a9edead2fa45c868d97d0b4e667911818
David <daviddenghaotian@163.com>
2018-12-03 02:28:59 -0500

    server.js using lout, added /members route to retrieve all members

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 // A controller that provides the API
 
-/*
+// /*
 // connecting database
 const knex = require('knex')({
 	client: 'pg',
     connection: {
         host: "faraday.cse.taylor.edu",
-        database: "",
-		user: "",
-		password: ""
+        database: "david_deng",
+		user: "david_deng",
+		password: "wowaqufa"
     }
 });
-*/
+// */
+
+// setting up ORM
+const { Model } = require('objection');
+Model.knex(knex);
+const Members = require('./src/ORM/Members');
+const Teams = require('./src/ORM/Teams');
+
 
 // setting up server
 const Hapi = require('hapi');
@@ -32,21 +39,39 @@ server.route([
 			return 'hello world';
 		}
 	},
+	{
+		method: "GET",
+		path: "/members",
+		config: {
+			description: "retrieve all users"
+		},
+		handler: async (request, h) => {
+			return Members
+				.query()
+				.select("*");
+		}
+	},
 	/* New route template
 	{
 		method: "",
 		path: "",
 		config: {
 			description: ""
-		}
+		},
 		handler: async (request, h) => {
 			return 0;
 		}
-	}
+	},
 	*/
 ]);
 
 async function init() {
+	// Configure plug-ins.
+    await server.register([
+        require('vision'),
+        require('inert'),
+        require('lout')
+    ]);
 	try {
 		await server.start();
 	}
@@ -55,6 +80,7 @@ async function init() {
 		process.exit(1);
 	}
 	console.log(`Server running at: ${server.info.uri}`);
+	console.log(`Route documentation available at ${server.info.uri}/docs`);
 }
 
 init();
