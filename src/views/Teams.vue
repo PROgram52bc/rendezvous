64b35ce777a88ed38a57c1b8e1cc668fd56b3412
David <daviddenghaotian@163.com>
2018-12-05 13:32:58 -0500

    My Teams view working

--- src/views/Teams.vue
+++ src/views/Teams.vue
@@ -1,5 +1,46 @@
 <template>
 	<div>
-
+		<div class="tabs is-centered is-toggle">
+			<ul>
+				<li class="is-active"><a>My Teams</a></li>
+				<li><a>All Teams</a></li>
+				<li><a>Create Team</a></li>
+			</ul>
+		</div>
+		<div class="box">
+			<keep-alive>
+				<component v-bind:is="currentComponent" v-bind="currentProperties"></component>
+			</keep-alive>
+		</div>
 	</div>
 </template>
+<script>
+import MyTeams from '../components/MyTeams.vue';
+export default {
+	data: function() { return {
+		currentComponent: MyTeams
+	}},
+	components: { MyTeams },
+	computed: {
+		currentProperties: function() {
+			if (this.currentComponent === MyTeams) {
+				return { 
+					teams: [
+						{
+							name: 'Team Alpha',
+							id: '1',
+							joined: true
+						},
+						{
+							name: 'Team Beta',
+							id: '2',
+							joined: false
+						},
+					]
+				}
+			}
+			else return this.currentComponent;
+		}
+	}
+}
+</script>
